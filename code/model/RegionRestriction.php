c38584a4dec5110a902ee9ffc76b531815fcb4ed
Jeremy Shipman <jeremy@burnbright.net>
2012-11-05 16:18:22 +1300

    NEW : Added summary fields to RegionRestriction. Tidied adderss_filter sql a bit. Commenting.

--- code/model/RegionRestriction.php
+++ code/model/RegionRestriction.php
@@ -18,6 +18,12 @@ class RegionRestriction extends DataObject{
 	
 	static $default_sort = "\"Country\" ASC, \"State\" ASC, \"City\" ASC, \"PostalCode\" ASC, \"Rate\" ASC";
 	
+	static $summary_fields = array(
+		'Country',
+		'State',
+		'City',
+		'PostalCode'
+	);
 	
 	/**
 	 * Produce a SQL filter to get matching RegionRestrictions to a given address
@@ -31,18 +37,21 @@ class RegionRestriction extends DataObject{
 			"PostalCode"
 		);
 		$where = array();
+		$rr = "\"RegionRestriction\"";
 		foreach($restrictables as $field){
-			$where[] = "TRIM(LOWER(\"$field\")) = TRIM(LOWER('".$address->$field."')) OR \"$field\" = '*' OR \"$field\" = ''";
+			$where[] = "TRIM(LOWER($rr.\"$field\")) = TRIM(LOWER('".$address->$field."')) OR $rr.\"$field\" = '*' OR $rr.\"$field\" = ''";
 		}
 		return "(".implode(") AND (", $where).")";
 	}
 	
 	function onBeforeWrite(){
+		//prevent empty data - '*' must be used
 		foreach(self::$defaults as $field => $value){
 			if(empty($this->$field)){
 				$this->$field = $value;
 			}
 		}
+		//TODO: prevent non-heirarichal entries, eg country = '*', state = 'blah'		
 		parent::onBeforeWrite();
 	}
 	
