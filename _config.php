0fa5ee6fba6494793362cab9b0c65eeaf3171d6f
Jeremy Shipman <jeremy@burnbright.net>
2012-11-12 11:14:09 +1300

    NEW: Allow PopulateTableShippingTask to be run independent of PopulateShopTask

--- _config.php
+++ _config.php
@@ -1,5 +1,5 @@
 <?php
 
-Object::add_extension("PopulateShopTask", "PopulateTableShippingTask");
+Object::add_extension("PopulateShopTask", "PopulateShopTableShippingTask");
 Object::add_extension("Order", "OrderShippingDecorator");
 Object::add_extension("CartPage_Controller", "CartPageDecorator");
--- code/tasks/PopulateTableShippingTask.php
+++ code/tasks/PopulateTableShippingTask.php
@@ -1,13 +1,30 @@
 <?php
 
-class PopulateTableShippingTask extends Extension{
+class PopulateTableShippingTask extends BuildTask{
 	
-	function beforePopulate(){
+	protected $title = "Populate Table Shipping Methods";
+	protected $description = 'If no table shipping methods exist, it creates multiple different setups of table shipping.';
+	
+	function run($request = null){
 		if(!DataObject::get_one('TableShippingMethod')){
 			$fixture = new YamlFixture('shop_shippingframework/tests/fixtures/TableShippingMethod.yml');
 			$fixture->saveIntoDatabase();
-			DB::alteration_message('Created shipping options', 'created');
+			DB::alteration_message('Created table shipping methods', 'created');
+		}else{
+			DB::alteration_message('Some table shipping methods already exist. None were created.');
 		}
+	}
+	
+}
+
+/**
+ * Makes PopulateTableShippingTask get run before PopulateShopTask is run
+ */
+class PopulateShopTableShippingTask extends Extension{
+	
+	function beforePopulate(){
+		$task = new PopulateTableShippingTask();
+		$task->run();
 	}
 	
 }
