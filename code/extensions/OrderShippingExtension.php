e02cd24856abb5ed4a10489dcccc2ad244782657
Jeremy Shipman <jeremy@burnbright.net>
2014-02-07 12:15:33 +1300

    FIX: order shipping extension fix

--- code/extensions/OrderShippingExtension.php
+++ code/extensions/OrderShippingExtension.php
@@ -1,24 +1,24 @@
 <?php
 
-class OrderShippingDecorator extends DataExtension{
+class OrderShippingExtension extends DataExtension{
 	
-	static $db = array(
+	private static $db = array(
 		'ShippingTotal' => 'Currency'
 	);
-	static $has_one = array(
+	private static $has_one = array(
 		'ShippingMethod' => 'ShippingMethod'
 	);
 	
-	function createShippingPackage(){
+	public function createShippingPackage() {
 		//create package, with total weight, dimensions, value, etc
 		$weight = $width = $height = $depth = $value = $quantity = 0;
 		
 		$items = $this->owner->Items();
 		
-		$weight = $items->Sum('Weight',true); //Sum is found on OrdItemList (Component Extension)
-		$width = $items->Sum('Width',true);
-		$height = $items->Sum('Height',true);
-		$depth = $items->Sum('Depth',true);
+		$weight = $items->Sum('Weight', true); //Sum is found on OrdItemList (Component Extension)
+		$width = $items->Sum('Width', true);
+		$height = $items->Sum('Height', true);
+		$depth = $items->Sum('Depth', true);
 		
 		$value = $this->owner->SubTotal();
 		$quantity = $items->Quantity();
