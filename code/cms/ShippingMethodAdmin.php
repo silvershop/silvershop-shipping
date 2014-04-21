db5a8da7e3aa089654c2bf5c7f3cbcd4c27bb0a5
Jeremy Shipman <jeremy@burnbright.net>
2014-04-21 18:23:11 +1200

    Updated ShipingMethodAdmin menu priority

--- code/cms/ShippingMethodAdmin.php
+++ code/cms/ShippingMethodAdmin.php
@@ -3,7 +3,7 @@ class ShippingMethodAdmin extends ModelAdmin{
 
 	private static $url_segment = "shipping";
 	private static $menu_title = "Shipping";
-	private static $menu_priority = 2;
+	private static $menu_priority = 3;
 	private static $menu_icon = 'shop_shipping/images/shipping.png';
 
 	private static $managed_models = array(
