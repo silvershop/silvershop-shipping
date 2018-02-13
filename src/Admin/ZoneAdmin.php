5d9b16bd116faaa2e90d2edf9e2989f109f5e557
Will Rossiter <will@fullscreen.io>
2018-02-13 17:26:41 +1300

    Missing image

--- src/Admin/ZoneAdmin.php
+++ src/Admin/ZoneAdmin.php
@@ -11,8 +11,6 @@ class ZoneAdmin extends ModelAdmin
 
     private static $url_segment = 'zones';
 
-    private static $menu_icon = 'silvershop/core:images/icons/local-admin.png';
-
     private static $menu_priority = 2;
 
     private static $managed_models = [
